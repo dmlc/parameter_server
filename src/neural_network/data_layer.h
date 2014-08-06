7994cf62fdad76be18c2ecb7581efc3354ce8a21
muli <muli@cs.cmu.edu>
2014-08-06 14:04:34 +0800

    data layer

--- src/neural_network/data_layer.h
+++ src/neural_network/data_layer.h
@@ -1,6 +1,7 @@
 #pragma once
-
 #include "neural_network/layer.h"
+#include "base/matrix_io.h"
+
 namespace PS {
 namespace NN {
 
@@ -8,16 +9,36 @@ template<typename V>
 class DataLayer : public Layer<V>  {
  public:
   void init() {
+    data_ = readMatrices<V>(cf_.data());
+    CHECK_EQ(data_.size(), cf_.out_size());
+    // TODO normalization
 
+    size_t n = data_[0]->rows();
+    for (size_t i = 0; ; ++i) {
+      size_t size = cf_.minibatch_size();
+      size = size == 0 ? n : size;
+      SizeR batch = SizeR(0, size) * i;
+      if (batch.end() > n) break;
+      batches_.push_back(batch);
+    }
   }
 
   // forward the data
   V forward() {
+    out_args_.resize(data_.size());
+    SizeR batch = batches_[rand() % batches_.size()];
+    for (int i = 0; i < data_.size(); ++i) {
+      out_args_.value = data_[i]->rowBlock(batch);
+    }
     return 0;
   }
 
   // do nothing
   void backward() {}
+ protected:
+  MatrixPtrList<V> data_;
+  std::vector<SizeR> batches_;
+  size_t batch_id_ = 0;
 };
 
 } // namespace NN
