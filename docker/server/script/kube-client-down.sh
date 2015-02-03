a2a4d5d5d97dd5af0dfcf14f696e24cd224af2b4
qicongc <qicongc@cs.cmu.edu>
2015-02-03 03:00:37 +0000

    sync with src change, add ssh-key support, fix bug

--- docker/server/script/kube-client-down.sh
+++ docker/server/script/kube-client-down.sh
@@ -3,6 +3,11 @@ while read line
 do
 	stop_all="docker stop \$(docker ps -a -q)"
 	rm_all="docker rm \$(docker ps -a -q)"
-	ssh ${line} ${stop_all} < /dev/null
-	ssh ${line} ${rm_all} < /dev/null
+	if [ $# -gt 0 ]; then
+		ssh -i $1 ${line} ${stop_all} < /dev/null
+		ssh -i $1 ${line} ${rm_all} < /dev/null
+	else
+		ssh ${line} ${stop_all} < /dev/null
+                ssh ${line} ${rm_all} < /dev/null
+	fi
 done
--- docker/server/script/kube-up.sh
+++ docker/server/script/kube-up.sh
@@ -3,4 +3,7 @@ serverip=`hostname -i`
 kube_server="./kube-server-up.sh"
 kube_client="./kube-client-up.sh" 
 cat ../host/minions_file | ${kube_server}
-cat ../host/minions_file | ${kube_client} ${serverip}
+if [ $# -gt 0 ] 
+then cat ../host/minions_file | ${kube_client} ${serverip} $1
+else cat ../host/minions_file | ${kube_client} ${serverip}
+fi
