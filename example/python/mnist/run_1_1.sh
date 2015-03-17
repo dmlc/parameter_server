d6be933162dac57671c98e2cc5cb43c472af83ba
Hyeontaek Lim <hl@cs.cmu.edu>
2015-03-17 14:25:05 -0400

    add local test scripts

---
+++ example/python/mnist/run_1_1.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+BASEDIR=$(dirname $0)/../../..
+
+export LD_LIBRARY_PATH=${BASEDIR}/../minerva/deps/lib:${BASEDIR}/third_party/lib:${LD_LIBRARY_PATH}
+export PYTHONPATH=${BASEDIR}/../minerva/release/owl:${BASEDIR}/../minerva/owl:$PYTHONPATH
+export PYTHONPATH=${BASEDIR}/src/app/python/:$PYTHONPATH
+
+scheduler="role:SCHEDULER,hostname:'127.0.0.1',port:8000,id:'H'"
+W0="role:WORKER,hostname:'127.0.0.1',port:8001,id:'W0'"
+S0="role:SERVER,hostname:'127.0.0.1',port:8010,id:'S0'"
+arg="-num_servers 1 -num_workers 1 -num_threads 1"
+bin=$BASEDIR/build/ps_python
+script="$1"
+shift
+
+for PID in $(ps aux | grep build/ps_python | grep -v grep | awk '{ print $2; }'); do
+  kill -9 $PID
+done
+
+
+$bin $arg -scheduler $scheduler -my_node $scheduler &
+$bin $arg -scheduler $scheduler -my_node $S0 -script "$script" -- "$@" &
+$bin $arg -scheduler $scheduler -my_node $W0 -script "$script" -- "$@"
+
