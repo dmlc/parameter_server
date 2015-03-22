6dd9873c12b59a61e10b09b5ff6d748ac0e45817
muli <muli@cs.cmu.edu>
2015-03-22 13:30:46 -0400

    commit before leaving for airport

--- src/util/shared_array.h
+++ src/util/shared_array.h
@@ -6,131 +6,242 @@
 #include "Eigen/Core"
 #include "data/proto/data.pb.h"
 #include "parameter/proto/param.pb.h"
-
 namespace PS {
 
 template<typename V> class Matrix;
-
-// static std::atomic<int64> g_mem_usage_sarray = ATOMIC_VAR_INIT(0);
-// extern int64 g_mem_usage_sarray;
-// extern std::mutex g_mu_sa_;
-
-// Memory efficient array. Most operations are zero-copy, such as assign, slice
-// a segment, convert to Eigen3 vector/array. It shares the same semantic as a C
-// array pointer. For example,
-//   SArray<int> A(10);
-//   SArray<int> B = A;
-//   SArray<int> C = A.segment(SizeR(1,3));
-//   A[2] = 2;
-// then B[2] == 2 and C[1] == 2 too.
+/**
+ * @brief Shared array
+ *
+ * It's a wrapper of C array point with std::shared_ptr. The data pointed is
+ * guaranteed to be deleted when the last SArray is deleted or reseted. It
+ * memory efficient. Most operations are zero-copy, such as assign, slice a
+ * segment, convert to Eigen3 vector/array. It shares the same semantic as a C
+ * array pointer. For example,
+ * \code{.cpp}
+   SArray<int> A(10);
+   SArray<int> B = A;
+   SArray<int> C = A.Segment(SizeR(1,3));
+   A[2] = 2;
+   CHECK_EQ(B[2], 2);
+   CHECK_EQ(C[1], 2);
+   \endcode
+ */
 template<typename V> class SArray {
  public:
   SArray() { }
   ~SArray() { }
-  // Create an array with length n. Values are not initialized. To initialize
-  // them, call setValue(v) or setZero()
+  /**
+   * @brief Create an array with length n.
+   *
+   * Values are not initialized. To initialize them, call SetValue() or SetZero()
+   */
   explicit SArray(size_t n) { resize(n); }
   SArray(size_t n, V val) { resize(n, val); }
 
-  // Zero-copy constructor, namely just copy the pointer
+  /**
+   * @brief Zero-copy constructor, namely just copy the pointer
+   */
   template <typename W> explicit SArray(const SArray<W>& arr);
+  /**
+   * @brief Zero-copy constructor, namely just copy the pointer
+   */
   template <typename W> void operator=(const SArray<W>& arr);
 
+  /**
+   * @brief Zero-copy constructor.
+   *
+   * @param data
+   * @param size
+   * @param deletable if true, the "data" will be deleted in the copy last
+   * SArray
+   */
   SArray(V* data, size_t size, bool deletable = true) {
     reset(data, size, deletable);
   }
 
-  // Copy constructors
+  /**
+   * @brief Copy constructor
+   *
+   * @param src
+   * @param size
+   */
   void CopyFrom(const V* src, size_t size);
+  /**
+   * @brief Copy constructor
+   *
+   * @param arr
+   */
   void CopyFrom(const SArray<V>& arr);
-  // A general but might slower version
+  /**
+   * @brief A general but might slower version of copy constructor
+   *
+   * @param first
+   * @param last
+   */
   template <typename ForwardIt>
   void CopyFrom(const ForwardIt first, const ForwardIt last);
-  // Copy from a initializer_list
+
+  /**
+   * Copy from a initializer_list
+   *
+   * @param list
+   */
   template <typename W> SArray(const std::initializer_list<W>& list);
+  /**
+   * Copy from a initializer_list
+   *
+   * @param list
+   */
   template <typename W> void operator=(const std::initializer_list<W>& list);
 
-
-  // Slice a [range.begin(), range.end()) segment, zero-copy
+  /**
+   * @brief Slice a segment, zero-copy
+   *
+   * @param range the index segment
+   * @return the segment [range.begin(), range.end())
+   */
   SArray<V> Segment(const Range<size_t>& range) const;
-  // Assume all arraies are ordered, return *this \cap other. for example:
-  //   SArray<int> a{1,2,3,5,6,7,8}, b{3,4,7,10}, c{3,7};
-  // then a.setIntersection(b) == c
+
+  /**
+   * @brief Performs set intersection between two sorted arrays
+   *
+   * An example:
+   \code{cpp}
+   SArray<int> a{1,2,3,5,6,7,8}, b{3,4,7,10}, c{3,7};
+   CHECK_EQ(a.SetIntersection(b), c);
+   \endcode
+   * @param other
+   *
+   * @return *this \f$\cap\f$ other
+   */
   SArray<V> SetIntersection(const SArray<V>& other) const;
-  // Assume all arraies are ordered, return *this \cup other. for example:
-  //   SArray<int> a{3,5,8,10}, b{5,9,10,11}, c{3,5,8,9,10,11};
-  // then a.setUnion(b) == c
+
+  /**
+   * @brief Perform set union between two sorted arrays
+   *
+   * An example:
+   \code{cpp}
+   SArray<int> a{3,5,8,10}, b{5,9,10,11}, c{3,5,8,9,10,11};
+   CHECK_EQ(a.SetUnion(b), c)
+   \endcode
+   * @param other
+   *
+   * @return *this \f$\cup\f$ other
+   */
   SArray<V> SetUnion(const SArray<V>& other) const;
+
+
   // Assume array values are ordered. return the position range of the segment
-  // whose entry values are within [bound.begin(), bound.end())
+  // whose entry values are within
+
+  /**
+   * @brief Find the index range of a segment of a sorted array such that the
+   * entries in this segment is within [bound.begin(), bound.end())
+   *
+   * An example
+   \code{cpp}
+   SArray<int> a{1 3 5 7 9};
+   CHECK_EQ(SizeR(1,3), a.FindRange(Range<int>(2,7)));
+   \endcode
+   * @param bound
+   *
+   * @return the index range
+   */
   SizeR FindRange (const Range<V>& bound) const;
 
-  // Capacity
   size_t size() const { return size_; }
   size_t capacity() const { return capacity_; }
+
+  /**
+   * @brief Returns the memory size in bytes
+   */
   size_t MemSize() const { return capacity_*sizeof(V); }
 
-  // static int64 gMemSize() { return g_mem_usage_sarray.load(); }
 
   bool empty() const { return size() == 0; }
-  // Replace the current data pointer with data. the memory associated with the
-  // replaced pointer will be released if no other SArray points to it.
+
+  /**
+   * @brief Reset the current data pointer
+   *
+   * @param data
+   * @param size
+   * @param deletable
+   */
   void reset(V* data, size_t size, bool deletable = true);
-  // Resizes the array so that it contains n elements.
-  // If n <= capacity_, then only change the size. otherwise, append n -
-  // current_size entries (without value initialization)
+
+  /**
+   * @brief Resizes the array to n elements
+   *
+   * If n <= capacity_, then only change the size. otherwise, append n -
+   * current_size entries (without value initialization)
+   * @param n
+   */
   void resize(size_t n);
+
+  /**
+   * @brief Resizes the array to n elements
+   *
+   * If n <= capacity_, then only change the size. otherwise, append n -
+   * current_size entries, and then set all value to val (TODO doesn't make sense)
+   * @param val
+   * @param n
+   */
   void resize(size_t n, V val) { resize(n); SetValue(val); }
-  // Requests that the capacity be at least enough to contain n elements.
+
+  /**
+   * @brief Requests that the capacity be at least enough to contain n elements.
+   *
+   * @param n
+   */
   void reserve(size_t n);
+
   void clear() { reset(nullptr, 0); }
 
-  // Iterators
   V* begin() { return data(); }
   const V* begin() const { return data(); }
   V* end() { return data() + size(); }
   const V* end() const { return data() + size(); }
 
-  // Element access:
   V back() const { CHECK(!empty()); return data_[size_-1]; }
   V front() const { CHECK(!empty()); return data_[0]; }
   V& operator[] (int i) { return data_[i]; }
   const V& operator[] (int i) const { return data_[i]; }
 
-  // Modifiers
   void append(const SArray<V>& tail);
   void push_back(const V& val);
   void pop_back() { if (size_) --size_; }
   void SetValue(V value);
-  // set all entries into 0
+
+  /// @brief set all entries into 0
   void SetZero() { memset(data_, 0, size_*sizeof(V)); }
-  // set values according to *cf*
+  //
+  /// @brief set values according to *cf*
   void SetValue(const ParamInitConfig& cf);
 
-  // Others
-  // Assume values are ordered, return the value range.
+  /// @brief  Assume values are ordered, return the value range.
   Range<V> range() const {
     return (empty() ? Range<V>(0,0) : Range<V>(front(), back()+1));
   }
   V* data() const { return data_; }
   const std::shared_ptr<void>& pointer() const { return ptr_; }
-  // number of non-zero entries
+
+  /// @brief the number of non-zero entries
   size_t nnz() const;
 
-  // Compare values
+  /// @brief Compare values
   template <typename W> bool operator==(const SArray<W> &rhs) const;
 
-  // return an Eigen3 vector, zero-copy
+  /// @brief return an Eigen3 vector, zero-copy
   typedef Eigen::Map<Eigen::Matrix<V, Eigen::Dynamic, 1> > EVecMap;
   EVecMap EigenVector() const { return EVecMap(data(), size()); }
   EVecMap vec() const { return EVecMap(data(), size()); }
 
-  // return an Eigen3 array, zero-copy
+  /// @brief return an Eigen3 array, zero-copy
   typedef Eigen::Map<Eigen::Array<V, Eigen::Dynamic, 1> > EArrayMap;
   EArrayMap EigenArray() const { return EArrayMap(data(), size()); }
   EArrayMap arr() const { return EArrayMap(data(), size()); }
 
-  // return an Eigen3 matrix, zero-copy
+  /// @brief return an Eigen3 matrix, zero-copy
   typedef Eigen::Map<Eigen::Array<V, Eigen::Dynamic, Eigen::Dynamic> > EMatMap;
   EMatMap EigenMatrix(int k) const {
     CHECK_EQ(size()%k, 0); return EArrayMap(data(), size()/k, k);
@@ -147,28 +258,28 @@ template<typename V> class SArray {
         (EigenArray() - Mean()).matrix().norm() / sqrt((double)size());
   }
 
-  // convert to a dense matrix, zero-copy
+  /// @brief convert to a dense matrix, zero-copy
   std::shared_ptr<Matrix<V>> SMatrix(size_t rows = -1, size_t cols = -1);
 
-  // Return the compressed array by snappy
+  /// @brief  Return the compressed array by snappy
   SArray<char> CompressTo() const;
-  // Uncompress the values from src with size src_size. Before calling this
-  // function, you should allocate enough memory first (e.g. call resize(xx))
+  /// @brief Uncompress the values from src with size src_size. Before calling this
+  /// function, you should allocate enough memory first (e.g. call resize(xx))
   void UncompressFrom(const char* src, size_t src_size);
   void UncompressFrom(const SArray<char>& src) { UncompressFrom(src.data(), src.size()); }
 
-  // read the segment [range.begin(), range.end()) from the binary file
+  /// @brief read the segment [range.begin(), range.end()) from the binary file
   bool ReadFromFile(SizeR range, const string& file_name);
   bool ReadFromFile(const string& file_name) {
     return ReadFromFile(SizeR::all(), file_name);
   }
   bool ReadFromFile(SizeR range, const DataConfig& file);
 
-  // write all values into a binary file
+  /// @brief  write all values into a binary file
   bool WriteToFile(const string& file_name) const {
     return WriteToFile(SizeR(0, size_), file_name);
   }
-  // write the segment [range.begin(), range.end()) into a binary file
+  /// @brief write the segment [range.begin(), range.end()) into a binary file
   bool WriteToFile(SizeR range, const string& file_name) const;
 
 
