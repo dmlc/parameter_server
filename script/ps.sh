05e454507d63da2b69cb9b4c3b89f4ca5e4fac90
muli <muli@cs.cmu.edu>
2015-03-27 13:44:31 -0400

    update ps.sh

--- script/ps.sh
+++ script/ps.sh
@@ -2,15 +2,15 @@
 
 # Part 1 : command parsing functions
 show_help() {
-cat << EOF
+    cat << EOF
 Usage: ps.sh [start|add|kill|stop] [args]
-start 		start an application
+start [options] bin args 	start an application
     -hostfile 	a list of ip address of machines. parameter server nodes are
 evenly assigned to these machines. #nodes could > #machines. if it is empty, generate a file with localhost ip
-    -worker     number of worker nodes
-    -server     number of server nodes
-    -bin     	binary to launch, eg:../build/linear
-    -arg 	other arguments, eg:"-app_file l1lr.conf"
+    -nw     number of worker nodes
+    -ns     number of server nodes
+    bin     	binary to launch, eg:../build/linear
+    args 	other arguments, eg: -app_file l1lr.conf
 
 ls 		print a list of running nodes with format "node_id server/worker/scheduler machine:port bin arg"
 
@@ -36,25 +36,19 @@ show_file_not_exist() {
 }
 
 check_start() {
-	if [[ ! $worker ]]; then
-	    show_empty '-worker'
-	fi
-	if [[ ! $server ]]; then
-	    show_empty '-server'
-	fi
-	if [[ ! $bin ]]; then
-	    show_empty '-bin'
-	fi
+    echo "use binary [$bin] and arg [$arg]"
+    echo "starting $server servers and $worker workers"
 	# if [[ ! $arg ]]; then
 	#     show_empty '-arg'
 	# fi
-	if [[ ! $hostfile ]];
-	then
+    local=0
+	if [[ ! $hostfile ]]; then
+        local=1
 	    platform=$(uname)
 	    if [[ $platform = 'Darwin' ]];then
 	    	echo '127.0.0.1' > ps_default_hostfile
-	    else 
-		echo $(hostname -i) > ps_default_hostfile
+	    else
+		    echo $(hostname -i) > ps_default_hostfile
 	    fi
 	    hostfile='ps_default_hostfile'
 	elif [[ ! -f $hostfile ]]; then
@@ -109,35 +103,32 @@ get_port() {
 }
 
 start_scheduler() {
-	# $1 host
-	get_port $1
-	# start scheduler
-	Sch="\"role:SCHEDULER,hostname:'$1',port:$port,id:'H'\""
-	ssh $1 "cd $(pwd) && $bin -my_node $Sch -scheduler $Sch -num_workers $worker -num_servers $server $arg &
-	echo -e \"H\tscheduler\t$1:$port\t"'$!'"\" >> /tmp/ps-state/nodes" &
-	echo "$bin -my_node $Sch -scheduler $Sch -num_workers $worker -num_servers $server $arg"
-	# save parameter to facilitate add operation
-	echo -e "$bin\t$Sch\t$server\t$worker\t$arg" > /tmp/ps-state/args
+    # $1 host
+    get_port $1
+    # start scheduler
+    Sch="\"role:SCHEDULER,hostname:'$1',port:$port,id:'H'\""
+    ssh  $1 "cd $(pwd) && $bin -my_node $Sch -scheduler $Sch -num_workers $worker -num_servers $server $arg & echo -e \"H\tscheduler\t$1:$port\t"'$!'"\" >> /tmp/ps-state/nodes" &
+    echo "$bin -my_node $Sch -scheduler $Sch -num_workers $worker -num_servers $server $arg"
+    # save parameter to facilitate add operation
+    echo -e "$bin\t$Sch\t$server\t$worker\t$arg" > /tmp/ps-state/args
 }
 
 start_worker() {
-	# $1 host, $2 id
-	get_port $1
-	# start scheduler
-	N="\"role:WORKER,hostname:'$1',port:$port,id:'W$2'\""
-	ssh $1 "cd $(pwd) && $bin -my_node $N -scheduler $Sch -num_workers $worker -num_servers $server $arg &
-	echo -e \"W$2\tworker\t$1:$port\t"'$!'"\" >> /tmp/ps-state/nodes" &
-	echo "$bin -my_node $N -scheduler $Sch -num_workers $worker -num_servers $server $arg"
+    # $1 host, $2 id
+    get_port $1
+    # start scheduler
+    N="\"role:WORKER,hostname:'$1',port:$port,id:'W$2'\""
+    ssh  $1 "cd $(pwd) && $bin -my_node $N -scheduler $Sch -num_workers $worker -num_servers $server $arg & echo -e \"W$2\tworker\t$1:$port\t"'$!'"\" >> /tmp/ps-state/nodes" &
+    echo "$bin -my_node $N -scheduler $Sch -num_workers $worker -num_servers $server $arg"
 }
 
 start_server() {
-	# $1 host, $2 id
-	get_port $1
-	# start scheduler
-	N="\"role:SERVER,hostname:'$1',port:$port,id:'S$2'\""
-	ssh $1 "cd $(pwd) && $bin -my_node $N -scheduler $Sch -num_workers $worker -num_servers $server $arg &
-	echo -e \"S$2\tserver\t$1:$port\t"'$!'"\" >> /tmp/ps-state/nodes" &
-	echo "$bin -my_node $N -scheduler $Sch -num_workers $worker -num_servers $server $arg"
+    # $1 host, $2 id
+    get_port $1
+    # start scheduler
+    N="\"role:SERVER,hostname:'$1',port:$port,id:'S$2'\""
+    ssh  $1 "cd $(pwd) && $bin -my_node $N -scheduler $Sch -num_workers $worker -num_servers $server $arg & echo -e \"S$2\tserver\t$1:$port\t"'$!'"\" >> /tmp/ps-state/nodes" &
+    echo "$bin -my_node $N -scheduler $Sch -num_workers $worker -num_servers $server $arg"
 }
 
 reset_counter() {
@@ -287,7 +278,8 @@ if [[ $# -lt 1 ]] ; then
 fi
 
 cmd=$1
-
+worker=1
+server=1
 
 if [[ $1 = 'start' ]]; then
 	shift
@@ -306,40 +298,22 @@ if [[ $1 = 'start' ]]; then
 	                show_empty '-hostfile'
 	            fi
 	            ;;
-	        -worker)
+	        -nw)
 	            if [[ $# -gt 1 ]] && ! [[ $2 = -?* ]]; then
 	                worker=$2
 	                shift 2
 	                continue
 	            else
-	                show_empty '-worker'
+	                show_empty '-nw'
 	            fi
 	            ;;
-	        -server)
+	        -ns)
 	            if [[ $# -gt 1 ]] && ! [[ $2 = -?* ]]; then
 	                server=$2
 	                shift 2
 	                continue
 	            else
-	                show_empty '-server'
-	            fi
-	            ;;
-	        -bin)
-	            if [[ $# -gt 1 ]] && ! [[ $2 = -?* ]]; then
-	                bin=$2
-	                shift 2
-	                continue
-	            else
-	                show_empty '-bin'
-	            fi
-	            ;;
-	        -arg)
-	            if [[ $# -gt 1 ]] ; then
-	                arg=$2
-	                shift 2
-	                continue
-	            else
-	                show_empty '-arg'
+	                show_empty '-ns'
 	            fi
 	            ;;
 	        -?*)
@@ -351,11 +325,25 @@ if [[ $1 = 'start' ]]; then
 
 	    shift
 	done
+
+	if [[ $# -gt 1 ]] && ! [[ $1 = -?* ]]; then
+	    bin=$1
+	    shift 1
+	    if [[ ! -f $bin ]]; then
+            echo "ERROR: binary [$bin] does not exist"
+            exit -1
+        fi
+	else
+        echo "ERROR: no binary file"
+        exit -1
+	fi
+    arg=$@
+
 	check_start
 	start
 	wait
-	clear
-	wait
+	# clear
+	# wait
 elif [[ $1 = 'stop' ]]; then
 	shift
 	while [[ $# -gt 0 ]]; do
@@ -401,7 +389,7 @@ elif [[ $1 = 'add' ]]; then
 	            show_help
 	            exit
 	            ;;
-	         -worker)
+	        -worker)
 	            if [[ $# -gt 1 ]] && ! [[ $2 = -?* ]]; then
 	                new_worker=$2
 	                shift 2
@@ -410,7 +398,7 @@ elif [[ $1 = 'add' ]]; then
 	                show_empty '-worker'
 	            fi
 	            ;;
-	         -server)
+	        -server)
 	            if [[ $# -gt 1 ]] && ! [[ $2 = -?* ]]; then
 	                new_server=$2
 	                shift 2
@@ -439,7 +427,7 @@ elif [[ $1 = 'kill' ]]; then
 	            show_help
 	            exit
 	            ;;
-	         H|W*|S*)
+	        H|W*|S*)
 	            node=$1
 	            shift 1
 	            ;;
